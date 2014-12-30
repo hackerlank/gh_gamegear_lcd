670ef43677c1bbd81c99b406ab727a8cb1cc548d
steward-fu <g9313716@yuntech.edu.tw>
2014-12-30 19:25:19 +0800

    update_20141230

--- GG_AV.vhd
+++ GG_AV.vhd
@@ -8,15 +8,14 @@ entity GG_AV is
 		red_out: 		out bit_vector(0 to 3);
 		green_out: 		out bit_vector(0 to 3);
 		blue_out: 		out bit_vector(0 to 3);
-		d: 				in bit_vector(0 to 3);
+		d_in:			in bit_vector(0 to 3);
 		csync: 			in std_logic;
 		hsync: 			in std_logic;
 		pix_clk: 		in std_logic;
 		sc_out: 		out std_logic;
-		csync_out: 		out std_logic;
-		oe: 			in std_logic;
-		vcc5: 			out std_logic;
-		d3_out: 		out std_logic
+		cs_out: 		out std_logic;
+		en_in: 			in std_logic;
+		en_out:			out std_logic
     );
 end GG_AV;
 
